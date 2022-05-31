b487e80c1bf5571e5f97612fcb59bfd020187e29
pszost <piotr.szost99@gmail.com>
2022-05-31 13:44:19 +0200

    Add polish translations and fix errors

--- src/Form/ConfigType.php
+++ src/Form/ConfigType.php
@@ -19,47 +19,59 @@ class ConfigType extends AbstractType
         $builder
             ->add('username', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.username',
             ])
             ->add('password', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.password',
             ])
             ->add('accountNumber', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.account_number',
             ])
             ->add('name', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.name',
             ])
             ->add('postalCode', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.post_code',
             ])
             ->add('city', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.city',
             ])
             ->add('street', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.street',
             ])
             ->add('houseNumber', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.house_number',
             ])
             ->add('phoneNumber', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.phone_number',
             ])
             ->add('email', TextType::class, [
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.email',
             ])
             ->add('payerType', ChoiceType::class, [
                 'choices' => [
-                    'Shipper' => PaymentData::PAYER_TYPE_SHIPPER,
-                    'Receiver' => PaymentData::PAYER_TYPE_RECEIVER,
+                    'bitbag.shopware_dhl_app.config.shipper' => PaymentData::PAYER_TYPE_SHIPPER,
+                    'bitbag.shopware_dhl_app.config.receiver' => PaymentData::PAYER_TYPE_RECEIVER,
                 ],
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.payer_type',
             ])
             ->add('paymentMethod', ChoiceType::class, [
                 'choices' => [
-                    'Bank transfer' => PaymentData::PAYMENT_METHOD_BANK_TRANSFER,
-                    'Cash' => PaymentData::PAYMENT_METHOD_CASH,
+                    'bitbag.shopware_dhl_app.config.bank_transfer' => PaymentData::PAYMENT_METHOD_BANK_TRANSFER,
+                    'bitbag.shopware_dhl_app.config.cash' => PaymentData::PAYMENT_METHOD_CASH,
                 ],
                 'required' => true,
+                'label' => 'bitbag.shopware_dhl_app.config.payment_method',
             ])
         ;
     }
