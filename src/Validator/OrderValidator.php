7eab974b748e4b5d7b5216c8fb18e11c814a6a69
pszost <piotr.szost99@gmail.com>
2022-07-13 14:57:21 +0200

    remove comment

--- src/Validator/OrderValidator.php
+++ src/Validator/OrderValidator.php
@@ -24,7 +24,7 @@ final class OrderValidator implements OrderValidatorInterface
         }
 
         if ('DHL' !== $technicalName) {
-            //throw new OrderException('bitbag.shopware_dhl_app.order.not_for_dhl');
+            throw new OrderException('bitbag.shopware_dhl_app.order.not_for_dhl');
         }
 
         if (!preg_match('/[0-9][0-9][-][0-9][0-9][0-9]/', $order->deliveries?->first()->shippingOrderAddress?->zipcode)) {
