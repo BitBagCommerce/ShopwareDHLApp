53d13b01369f9f85aee2816f3f92c08bc0e0d10b
pszost <piotr.szost99@gmail.com>
2022-07-15 16:47:27 +0200

    Fix package weight issue

--- src/Factory/PieceFactory.php
+++ src/Factory/PieceFactory.php
@@ -16,13 +16,22 @@ final class PieceFactory implements PieceFactoryInterface
     public function create(array $customFields, float $totalWeight): array
     {
         return (new Piece())
-            ->setType(Piece::TYPE_PACKAGE)
+            ->setType($this->getPieceType($totalWeight))
             ->setWidth($customFields[Defaults::PACKAGE_WIDTH])
             ->setHeight($customFields[Defaults::PACKAGE_HEIGHT])
             ->setLength($customFields[Defaults::PACKAGE_DEPTH])
-            ->setWeight((int) $totalWeight)
+            ->setWeight((int) (round($totalWeight)))
             ->setQuantity(1)
             ->setNonStandard(false)
             ->structure();
+    }
+
+    private function getPieceType(float $totalWeight): string
+    {
+        if (1.0 > $totalWeight) {
+            return Piece::TYPE_ENVELOPE;
+        }
+
+        return Piece::TYPE_PACKAGE;
     }
 }
