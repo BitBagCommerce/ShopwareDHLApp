eb225201cdd70362c5cca85464ad080f1db5f0d1
pszost <piotr.szost99@gmail.com>
2022-07-18 12:57:37 +0200

    Set country code permanently

--- src/Factory/ReceiverAddressFactory.php
+++ src/Factory/ReceiverAddressFactory.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace BitBag\ShopwareDHLApp\Factory;
 
 use Alexcherniatin\DHL\Structures\ReceiverAddress;
-use BitBag\ShopwareDHLApp\Provider\Defaults;
 use Vin\ShopwareSdk\Data\Entity\OrderAddress\OrderAddressEntity;
 
 final class ReceiverAddressFactory implements ReceiverAddressFactoryInterface
 {
+    private const RECEIVER_COUNTRY_CODE = 'PL';
+
     public function create(
         OrderAddressEntity $shippingAddress,
         string $customerEmail,
@@ -21,7 +22,7 @@ final class ReceiverAddressFactory implements ReceiverAddressFactoryInterface
 
         return (new ReceiverAddress())
             ->setAddressType(ReceiverAddress::ADDRESS_TYPE_B)
-            ->setCountry($customFields[Defaults::PACKAGE_COUNTRY_CODE])
+            ->setCountry(self::RECEIVER_COUNTRY_CODE)
             ->setName($shippingAddress->firstName . ' ' . $shippingAddress->lastName)
             ->setPostalCode($shippingAddress->zipcode)
             ->setCity($shippingAddress->city)
